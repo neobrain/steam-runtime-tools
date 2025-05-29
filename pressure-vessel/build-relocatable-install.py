neobrain/steam-runtime-tools

commit 17ba613bb813c0f67ecf0358c86ed9a65d42ef1b

build-relocatable-install: Include version number in copyright file names
The x86 build of pressure-vessel is still done in a scout environment
for maximum backwards-compatibility (although this will become
unnecessary in August, when Steam officially stops supporting glibc
older than 2.31) but the aarch64 build is done in a bullseye environment.
This means we'll have two different versions of each library: the
one from scout, and the one from bullseye.

steamrt/tasks#708

Signed-off-by: Simon McVittie <smcv@collabora.com>

pressure-vessel/build-relocatable-install.py
@@ -520,25 +520,7 @@ def main():
         for package, source in sorted(get_source):
             if os.path.exists('/usr/share/doc/{}/copyright'.format(package)):
                 installed_binaries.add(package)
-
-                if source in DIFFERENT_COPYRIGHT_FILES:
-                    install(
-                        '/usr/share/doc/{}/copyright'.format(package),
-                        os.path.join(
-                            installation,
-                            'metadata',
-                            '{}.txt'.format(package),
-                        ),
-                    )
-                else:
-                    install(
-                        '/usr/share/doc/{}/copyright'.format(package),
-                        os.path.join(
-                            installation,
-                            'metadata',
-                            '{}.txt'.format(source),
-                        ),
-                    )
+                source_version = ''
 
                 for expr in set(
                     v_check_output([
@@ -548,14 +530,44 @@ def main():
                         package,
                     ], universal_newlines=True).splitlines()
                 ):
-                    source_to_download.add(
-                        re.sub(r'[+]srt[0-9a-z.]+$', '', expr))
+                    without_build_suffix = re.sub(
+                        r'[+]srt[0-9a-z.]+$',
+                        '',
+                        expr,
+                    )
+                    source_to_download.add(without_build_suffix)
+                    after_equals = without_build_suffix.split('=', 1)[1]
+                    # We can only have one ${source:Version} installed
+                    assert source_version in ('', after_equals)
+                    source_version = after_equals
+
+                if source in DIFFERENT_COPYRIGHT_FILES:
+                    install(
+                        '/usr/share/doc/{}/copyright'.format(package),
+                        os.path.join(
+                            installation,
+                            'metadata',
+                            '{}_{}.txt'.format(package, source_version),
+                        ),
+                    )
+                else:
+                    install(
+                        '/usr/share/doc/{}/copyright'.format(package),
+                        os.path.join(
+                            installation,
+                            'metadata',
+                            '{}_{}.txt'.format(source, source_version),
+                        ),
+                    )
             else:
+                maybe_version = ''
+
                 if source == 'steam-runtime-tools':
                     copyright_file = os.path.join(
                         srcdir, 'debian', 'copyright',
                     )
                     source = source + '=' + args.version
+                    maybe_version = '_' + args.version
                 else:
                     copyright_file = os.path.join(
                         tmpdir,
@@ -569,7 +581,7 @@ def main():
                     os.path.join(
                         installation,
                         'metadata',
-                        '{}.txt'.format(source),
+                        '{}{}.txt'.format(source, maybe_version),
                     ),
                 )
                 source_to_download.add(source)

commit 269e6cf3262924433a007c2164e507a363011ad5

relocatable-install: Avoid epoch in copyright filenames
We're assuming here that source package names are always OK, but Debian
package version numbers can contain an epoch marker (starts with `1:`)
which introduce characters into the filename that are not compatible
with Windows filesystems or Steampipe.

Debian packages' versions are designed to be unique even when the epoch
is omitted, and the epoch is conventionally excluded from filenames
(for example the .dsc files that this script also handles) in order to
make them compatible with Windows filesystems.

steamrt/tasks#746

Fixes: 17ba613b "build-relocatable-install: Include version number in copyright file names"
Signed-off-by: Simon McVittie <smcv@collabora.com>

pressure-vessel/build-relocatable-install.py
@@ -541,13 +541,17 @@ def main():
                     assert source_version in ('', after_equals)
                     source_version = after_equals
 
+                # Omit the optional epoch marker if present, like
+                # dpkg-buildpackage does, to avoid ':' in filenames
+                safe_version = re.sub(r'^[0-9]+:', '', source_version)
+
                 if source in DIFFERENT_COPYRIGHT_FILES:
                     install(
                         '/usr/share/doc/{}/copyright'.format(package),
                         os.path.join(
                             installation,
                             'metadata',
-                            '{}_{}.txt'.format(package, source_version),
+                            '{}_{}.txt'.format(package, safe_version),
                         ),
                     )
                 else:
@@ -556,7 +560,7 @@ def main():
                         os.path.join(
                             installation,
                             'metadata',
-                            '{}_{}.txt'.format(source, source_version),
+                            '{}_{}.txt'.format(source, safe_version),
                         ),
                     )
             else: