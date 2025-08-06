9d4d83a501c665fccce8e038801d3c8c23f6b00d
Simon McVittie <smcv@collabora.com>
2025-08-06 15:36:16 +0100

    CI: Use Arch's multilib-devel image instead of reinventing it
    Thanks: Ludovico de Nittis
    Signed-off-by: Simon McVittie <smcv@collabora.com>

--- ci/prepare-arch.sh
+++ ci/prepare-arch.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
-# Copyright 2022 Collabora Ltd.
+# Copyright 2022-2025 Collabora Ltd.
 # SPDX-License-Identifier: MIT
 
 set -eux
 
-# Enable multilib repository
-echo -e "\n[multilib]\nInclude = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
-
 pacman -Syu --needed --noconfirm --noprogressbar \
-base-devel \
 git \
-lib32-glibc \
 lib32-mesa \
 libxrandr \
 python \
@@ -18,5 +13,4 @@ python-chardet \
 python-debian \
 python-six \
 python-tappy \
-sudo \
 "$@"
